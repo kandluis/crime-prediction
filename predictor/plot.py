2f76f1436bbaa6ab66765e0a0017c38fc4aeee4a
Luis Perez <luisperez@college.harvard.edu>
2015-12-04 03:57:39 -0500

    Improving plotting functionality

--- predictor/plot.py
+++ predictor/plot.py
@@ -42,19 +42,72 @@ def plotDistribution(predict, true, city, n, process='GP'):
     minValue = min(len(predict), 100)
     yPred = predict[-minValue:]
     yTrue = true[-minValue:]
+
+    # Plot Crime for Final Time Period
+    plt.clf()
+    plt.plot(yPred, label="Predictions")
+    plt.plot(yTrue, label="Actual Data")
+    plt.title('Crimes using {} in Last Time Period'.format(process))
+    plt.xlabel('Final {}} Regions'.format(minValue))
+    plt.ylabel('Crime Count')
+    plt.legend()
+    savefile = os.path.abspath('figures/{}_results/{}_crime_n={}_periods=last.png'.format(
+        city, process, n))
+    plt.savefig(savefile)
+    plt.close()
+
+    print "Crimes for final period saved to {}".format(savefile)
+
+    # Plot crime distribution for final period
     yPred = yPred / float(np.sum(yPred))
     yTrue = yTrue / float(np.sum(yTrue))
     plt.clf()
     plt.plot(yPred, label="Predictions")
     plt.plot(yTrue, label="Actual Data")
-    plt.title('Predictive Distribution for {}'.format(process))
-    plt.xlabel('Compressed Features')
+    plt.title('Predictive Distribution using {} in Last Time Period'.format(process))
+    plt.xlabel('Final {}} Regions'.format(minValue))
     plt.ylabel('Probability')
     plt.legend()
-    savefile = os.path.abspath('figures/{}_results/{}_n={}_periods={}.png'.format(
-        city, process, n, 12))
+    savefile = os.path.abspath('figures/{}_results/{}_dist_n={}_periods=last.png'.format(
+        city, process, n))
     plt.savefig(savefile)
     plt.close()
+
+    print "Distribution saved to {}".format(savefile)
+
+    yPred = predict[:minValue]
+    yTrue = true[:minValue]
+
+    # Plot Crime for First Time Period
+    plt.clf()
+    plt.plot(yPred, label="Predictions")
+    plt.plot(yTrue, label="Actual Data")
+    plt.title('Crimes using {} in First Time Period'.format(process))
+    plt.xlabel('Final {}} Regions'.format(minValue))
+    plt.ylabel('Crime Count')
+    plt.legend()
+    savefile = os.path.abspath('figures/{}_results/{}_crime_n={}_periods=first.png'.format(
+        city, process, n))
+    plt.savefig(savefile)
+    plt.close()
+
+    print "Crimes for first period saved to {}".format(savefile)
+
+    yPred = yPred / float(np.sum(yPred))
+    yTrue = yTrue / float(np.sum(yTrue))
+    plt.clf()
+    plt.plot(yPred, label="Predictions")
+    plt.plot(yTrue, label="Actual Data")
+    plt.title(
+        'Predictive Distribution using {} in First Time Period'.format(process))
+    plt.xlabel('Final 100 Regions')
+    plt.ylabel('Probability')
+    plt.legend()
+    savefile = os.path.abspath('figures/{}_results/{}_dist_n={}_period=first.png'.format(
+        city, process, n))
+    plt.savefig(savefile)
+    plt.close()
+
     print "Distribution saved to {}".format(savefile)
 
 
@@ -77,9 +130,12 @@ def plotHeatMaps(X_test, predict, city, n, process='GP'):
         selected = (X_test[:, columns['t']] == month)
         if selected.sum() > 0:
             plt.clf()
-            m = createHeatMap(trueValues[selected, :])
-            if m.sum() > 0:
-                sns.heatmap(m)
+            m1 = createHeatMap(trueValues[selected, :])
+            m2 = createHeatMap(predictedValues[selected, :])
+
+            # Make a plot only if both have data available :)
+            if m1.sum() > 0 and m2.sum > 0:
+                sns.heatmap(m1)
                 plt.title('True Density Distribution in Month {}'.format(month))
                 savefile = os.path.abspath('figures/{}_results/{}_heatmap_true_n={}_t={}.png'.format(
                     city, process, n, month))
@@ -87,10 +143,7 @@ def plotHeatMaps(X_test, predict, city, n, process='GP'):
                 plt.close()
                 print "True heatmap saved to {}".format(savefile)
 
-            plt.clf()
-            m = createHeatMap(predictedValues[selected, :])
-            if m.sum() > 0:
-                sns.heatmap(m)
+                sns.heatmap(m2)
                 plt.title(
                     'Predicted Density Distribution in Month {}'.format(month))
                 savefile = os.path.abspath('figures/{}_results/{}_heatmap_pred_n={}_t={}.png'.format(
